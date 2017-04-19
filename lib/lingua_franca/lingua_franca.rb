c9dc000c2e85f2063b1a5741e3fbeba3ffaf220a
Godwin <goodgodwin@hotmail.com>
2017-04-19 13:31:05 -0700

    Fixed another bug in versioning

--- lib/lingua_franca/lingua_franca.rb
+++ lib/lingua_franca/lingua_franca.rb
@@ -159,7 +159,7 @@ module LinguaFranca
     end
 
     def test_version(app_slug, app_path)
-      File.read(File.join(records_dir, '.version'))
+      File.read(File.expand_path(File.join(app_path, records_dir, '.version')))
     end
 
     def get_html(distance_from_root = 4)
